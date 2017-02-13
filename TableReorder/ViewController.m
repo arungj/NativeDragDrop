5382a3cca856599343b6fa5ab017d34327c9bd47
Arun Jasmine <ajasmine@nexient.com>
2017-02-13 16:21:48 -0500

    Test commit 1

--- TableReorder/ViewController.m
+++ TableReorder/ViewController.m
@@ -59,7 +59,7 @@
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
     UIAlertController *controller = [UIAlertController alertControllerWithTitle:@"Alert" message:[NSString stringWithFormat:@"selected row:%ld", indexPath.row] preferredStyle:UIAlertControllerStyleAlert];
     UIAlertAction *cancelAction = [UIAlertAction actionWithTitle:@"Cancel" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
-        
+        NSLog(@"test commit");
     }];
     [controller addAction:cancelAction];
     [self showViewController:controller sender:nil];
