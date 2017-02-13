02a05174a2fbb0983c97c3fa83f110bc1308e4f2
Arun Jasmine <ajasmine@nexient.com>
2017-02-13 12:09:52 -0500

    Added test cases 1) test app title 2) test list numbering

--- TableReorderTests/TableReorderTests.m
+++ TableReorderTests/TableReorderTests.m
@@ -8,6 +8,7 @@
 
 #import <XCTest/XCTest.h>
 #import "ViewController.h"
+#import "MainTableViewCell.h"
 
 @interface TableReorderTests : XCTestCase
 
@@ -30,6 +31,22 @@
     ViewController *vc = (id)w.rootViewController;
     NSInteger numRows = [vc.mainTableview numberOfRowsInSection:0];
     XCTAssertTrue(numRows > 0, @"Table should have non zero rows");
+}
+
+- (void) testFirstRow {
+    UIWindow *w = [UIApplication sharedApplication].keyWindow;
+    ViewController *vc = (id)w.rootViewController;
+    MainTableViewCell *cell = [vc.mainTableview cellForRowAtIndexPath:[NSIndexPath indexPathForRow:0 inSection:0]];
+    NSString *cellText = cell.label.text;
+    XCTAssertTrue([cellText hasSuffix:@"1"], @"list numbering should start with 1");
+}
+
+- (void) testAppTitle {
+    UIWindow *w = [UIApplication sharedApplication].keyWindow;
+    ViewController *vc = (id)w.rootViewController;
+    UINavigationBar *navBar = [[vc.view subviews] objectAtIndex:0];
+    NSString *appTitle = navBar.topItem.title;
+    XCTAssertTrue([appTitle isEqualToString:@"Table Reorder"] , @"App title should be Table Reorder");
 }
 
 @end
