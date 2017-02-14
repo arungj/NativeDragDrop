85d5225463bbda48e8115d7615d0646b96a13d0f
Arun Jasmine <ajasmine@nexient.com>
2017-02-14 10:01:08 -0500

    add app title check

--- TableReorderTests/TableReorderTests.m
+++ TableReorderTests/TableReorderTests.m
@@ -41,7 +41,7 @@
     XCTAssertTrue([cellText hasSuffix:@"1"], @"list numbering should start with 1");
 }
 
-- (void) estAppTitle {
+- (void) testAppTitle {
     UIWindow *w = [UIApplication sharedApplication].keyWindow;
     ViewController *vc = (id)w.rootViewController;
     UINavigationBar *navBar = [[vc.view subviews] objectAtIndex:0];
