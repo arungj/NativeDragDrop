a9a0fc09ba303681f435b381ed3571ac7a6de770
Arun George <arungj1989@gmail.com>
2017-04-23 17:06:16 -0700

    comment table view cell

--- TableReorder/ViewController.m
+++ TableReorder/ViewController.m
@@ -35,7 +35,7 @@
     MainTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell"];
     cell.label.text = [NSString stringWithFormat:@"Label %ld", indexPath.row+1];
     cell.label.textColor = [UIColor darkGrayColor];
-    
+    NSLog(@"in table view cell");
     return cell;
 }
 
